5517d9b9fbbc398af9166a47e428779936299bed
Hamza Zoumhani <hamza.zoumhani@okstate.edu>
2019-01-30 19:50:17 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -13,13 +13,33 @@ int main(){
 	short luke = 7;
 	float pie = 3.14;
 	char middle = 'M';
+	bool shouldWe = true;
 	
-
+	if (shouldWe){
+		cout << "The result was true!" << endl;
+	} else{
+		cout << "False!!!" << endl;
+	}
+	
+	if (johnbob > luke) {
+		cout << "The result was true!" << endl;
+	} else{
+		cout << "False!!!" << endl;
+	}
+	
     cout << "Bob is " << bob << " years old." << endl;
 	cout << "Bob and john together are " << johnbob << " years old." << endl;
 	cout << middle << " is the middle initial for ";
 	cout << pie << " users that are " << luke;
 	cout << " years old.";
-
+	
+	cout << johnbob << " is bob and john added." << endl;
+	cout << bob - john << " is bob and john susbtracted." << endl;
+	cout << bob/john << " is bob and john divided." << endl;
+	cout << bob*john << " is bob and john multiplied." << endl;
+	cout << bob%john << " is bob modulus john." << endl;
+	
+	
+	
     return 0;
 }
