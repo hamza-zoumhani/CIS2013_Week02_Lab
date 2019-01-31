565b139070b4a6ded2d188000c9b537e0abe99f0
Hamza Zoumhani <hamza.zoumhani@okstate.edu>
2019-01-30 19:27:44 -0600

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "Week 2 Lab" << endl;
+    int bob;
+    int john;
+
+    bob = 42;
+	john = 32;
+	int johnbob = bob+john;
+	short rachel = 10;
+	short luke = 7;
+	float pie = 3.14;
+	char middle = 'M';
+	
+
+    cout << "Bob is " << bob << " years old." << endl;
+	cout << "Bob and john together are " << johnbob << " years old." << endl;
+	cout << middle << " is the middle initial for ";
+	cout << pie << " users that are " << luke;
+	cout << " years old.";
+
+    return 0;
+}
